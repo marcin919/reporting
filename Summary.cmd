d2ef5775db7deff78e54782caff71bf4966d1680
marcin919 <mar.szmyd@gmail.com>
2020-02-11 23:36:27 +0100

    Add files via upload

--- Summary.cmd
+++ Summary.cmd
@@ -1,9 +1,6 @@
 chcp 65001
-
-REM 
 REM 	Windows Reporting Tool
-REM 	Version 02.2020.03 by marcin919
-REM 
+REM 	Version 2020-02-11 by marcin919
 @echo off
 
 mkdir %COMPUTERNAME%
@@ -12,51 +9,24 @@ set Network=%cd%\Network.log
 set /a stunde=%time:~0,2%
 set minute=%time:~3,2%
 
-REM NETWORK
-color 6a&&cls&&title by marcin919
-echo NETWORKING/NETZWEK >>"%Network%"
+
+REM 1. NETWORK
+
+color E1&&cls&&title Windows Reporting Tool (2020-02-11)
+echo NETZWEK >>"%Network%"
 echo Auswertung wurde startet um : %date% %stunde%:%minute%  >"%Network%" 
 		
 	ipconfig /all >>"%Network%" 
 	ping 1.1.1.1 >>"%Network%"
 	ping 8.8.8.8 >>"%Network%"
 
-REM Kann extra eingeschaltet werden. Braucht aber extrem viel Zeit.
+REM Extra Netzwerk auswertung. Sckript Braucht viel mehr Zeit.
 REM tracert 1.1.1.1 >>"%Network%"	
 REM pathping 1.1.1.1 >>"%Network%"
 
-REM USER
-color 2f
-Set User=%cd%\User.log
-	
-	whoami  >"%User%"
-	whoami /all >>"%User%"
-	net localgroup Administratoren >>"%User%"
-
-REM KEY
-REM https://www.tech-faq.net/windows-product-key-auslesen/
-color 3f
-Set Key=%cd%\Key.log
-	wmic path softwarelicensingservice get OA3xOriginalProductKey >"%Key%"
-
-REM SYSTEMINFO
-color 4f
-Set Systeminfo=%cd%\Systeminfo.log
-
-	systeminfo > %Systeminfo%
-
-REM PUBLIC IP 
-REM wird IPv6 unterstuezt? Google DNS IPv6
-REM ping -6 -n 1 COMPUTERNAME | findstr TTL && start home.mp3 || start alarm.mp3
-
-REM ping -6 -n 1 2001:4860:4860::8888 | findstr "(0% Verlust)" && call Public-IPv6.vbs || call Public-IPv4.vbs
-call Public-IPv4.vbs
-
-REM Summary
-color 5a
 set Summary=%cd%\%COMPUTERNAME%\Summary_%COMPUTERNAME%_%date%.log
 
-echo -------------------------------------------------------------  >"%Summary%"
+echo.>"%Summary%"
 findstr "Windows-IP-Konfiguration" Network.log >>"%Summary%"
 echo ------------------------  >>"%Summary%"
 findstr "Hostname" Network.log >>"%Summary%"
@@ -66,24 +36,39 @@ findstr "Subnetzmaske" Network.log >>"%Summary%"
 findstr "Standardgateway" Network.log >>"%Summary%"
 findstr "DHCP-Server" Network.log >>"%Summary%"
 findstr "DNS-Server" Network.log >>"%Summary%"
-echo.
-color 5f
-echo -------------------------------------------------------------  >>"%Summary%"
+echo.>>"%Summary%"
+
+
+REM 2. USER
+
+color D1
+Set User=%cd%\User.log
+	
+	whoami  >"%User%"
+	whoami /all >>"%User%"
+	net localgroup Administratoren >>"%User%"
+
+
 findstr "BENUTZERINFORMATIONEN" User.log >>"%Summary%"
 echo ---------------------  >>"%Summary%"
 	whoami  >>"%Summary%"
-echo.
-color 6c
-echo -------------------------------------------------------------  >>"%Summary%"
-echo PUBLIC IP-Adresse: >>"%Summary%"
-echo ------------------  >>"%Summary%"
-setlocal
-for /f "tokens=13* delims=<>, " %%a in ('findstr "<body>" Public-IP.txt')  do (
-	echo %%a >>"%Summary%"
-)
-echo.
-color ea
-echo -------------------------------------------------------------  >>"%Summary%"
+echo.>>"%Summary%"
+
+
+REM 3. KEY
+REM Funktioniert selten?!
+color C1
+Set Key=%cd%\Key.log
+	wmic path softwarelicensingservice get OA3xOriginalProductKey >"%Key%"
+REM ohne Ausgabe in >>"%Summary%"
+
+REM 4. SYSTEMINFO
+
+color B1
+Set Systeminfo=%cd%\Systeminfo.log
+	systeminfo > %Systeminfo%
+
+
 echo SYSTEMINFO >>"%Summary%"
 echo ---------- >>"%Summary%"
 setlocal
@@ -92,11 +77,34 @@ for /f "tokens=3* delims=, " %%a in ('systeminfo.exe ^| find /i "Urspr"') do (
 	set InstallTime=%%b
 )
 echo Ursprüngliches Installationsdatum: %InstallDate% >>"%Summary%"
+echo.>>"%Summary%"
 REM Unbrauchbar
 :: echo Ursprüngliches Installationszeit: %InstallTime% >>"%Summary%"
 
-color ac
-echo -------------------------------------------------------------  >>"%Summary%"
+
+REM 5. PUBLIC IP 
+
+color A1
+REM wird IPv6 unterstuezt? Google DNS IPv6 wird hier angepingt
+
+ping -6 -n 1 2001:4860:4860::8888 | findstr "(0% Verlust)" && call Public-IPv6.vbs || call Public-IPv4.vbs
+REM call Public-IPv4.vbs
+REM call Public-IPv6.vbs 
+
+
+echo PUBLIC IP-Adresse: >>"%Summary%"
+echo ------------------  >>"%Summary%"
+setlocal
+for /f "tokens=13* delims=<>, " %%a in ('findstr "<body>" Public-IP.txt')  do (
+	echo %%a >>"%Summary%"
+)
+echo.>>"%Summary%"
+
+
+REM 6. HARDWARE INFORMATIONEN 
+
+color 91
+
 echo HARDWARE INFORMATIONEN >>"%Summary%"
 echo ---------- >>"%Summary%"
 	powershell -Command "gwmi win32_baseboard | fl *" | findstr /B ".*Manufacturer">>"%Summary%"
@@ -105,9 +113,13 @@ echo ---------- >>"%Summary%"
 	powershell -Command "gwmi win32_baseboard | fl *" | findstr /B ".*SerialNumber">>"%Summary%"
 REM BIOS Serial Nummer, ziemmlich unbrauchbar. 
 ::	echo BIOS SN: | wmic bios get serialnumber >>"%Summary%"
-::	echo.>>"%Summary%"
-color fa
-echo -------------------------------------------------------------  >>"%Summary%"
+echo.>>"%Summary%"
+
+
+REM 7. OS INFORMATIONEN 
+
+color 8f
+
 echo OS INFORMATIONEN >>"%Summary%"
 echo ---------- >>"%Summary%"
 Set Slmgr=%cd%\Slmgr.log
@@ -117,8 +129,12 @@ findstr /b "Betriebssystemversion" Systeminfo.log >>"%Summary%"
 cscript c:\Windows\System32\slmgr.vbs /dli >"%Slmgr%"
 findstr "Teil-Product" Slmgr.log >>"%Summary%"
 findstr "Lizenzstatus" Slmgr.log >>"%Summary%"
-echo .
-color 5f
+echo.>>"%Summary%"
+
+
+REM 8. OS UPDATES 
+
+color 7f
 echo UPDATES >>"%Summary%"
 echo ---------- >>"%Summary%"
 Set Updates=%cd%\Updates.log
@@ -132,8 +148,9 @@ powershell -command "Get-HotFix | Sort-Object InstalledOn">>"%Summary%"
 REM setLocal enableDelayedExpansion
 REM for /f %%a in (Updates.log) do for /f "tokens=*" %%A in ('findstr /xs "%%a"') do set lastFound=%%A
 REM echo !lastFound! >>"%Summary%"
+echo.>>"%Summary%"
 
-color 9a
+color 6f
 REM /////////////////////////////////////////////////////////////////////////////////
 move .\Key.log ".\%COMPUTERNAME%\Key_%COMPUTERNAME%_%date%.log"
 move .\Network.log ".\%COMPUTERNAME%\Network_%COMPUTERNAME%_%date%.log"
